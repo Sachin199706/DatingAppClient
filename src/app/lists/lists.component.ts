78faedb4e797a4e16abea96f8dee1f5f66089958
Singh, Sachin <sachin.singh@sagitec.com>
2025-12-28 19:27:03 +0530

    #16

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { LikesService } from '../_Services/likes.service';
+import { Member } from '../Models/member';
+import { ButtonRadioDirective } from "ngx-bootstrap/buttons";
+import { FormsModule } from '@angular/forms';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { NgClass } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [ButtonRadioDirective, FormsModule, MemberCardComponent,PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit,OnDestroy  {
+   _likesServices: LikesService = inject(LikesService);
+  predicate = 'liked';
+  pageNumber:number=1;
+  pageSize:number=5;
+
+  ngOnInit(): void {
+    this.loadlikes();
+  }
+
+  getTitle(){
+    switch(this.predicate){
+        case 'liked': return 'Members you like';
+        case 'likedBy': return 'Members how like you';
+        default : return 'Mutual';
+    }
+  }
+
+  loadlikes(){
+    this._likesServices.getLikes(this.predicate,this.pageNumber,this.pageSize);
+  }
+
+  pageChanged(event:any){
+    if(this.pageNumber !==event.page){
+      this.pageNumber=event.page;
+      this.loadlikes();
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this._likesServices.paginatedResult.set(null);
+  }
 
 }
--- src/app/members/member-card/member-card.component.ts
+++ src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../Models/member';
 import { RouterLink } from "@angular/router";
+import { LikesService } from '../../_Services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,7 +13,21 @@ import { RouterLink } from "@angular/router";
 export class MemberCardComponent {
 //@Input() member!: Member;
 
-
+private _likesServices=inject(LikesService);
 member=input.required<Member>();
+hasLiked=computed(()=>this._likesServices.likeIds().includes(this.member().id));
+
+toggleLike(){
+  this._likesServices.toggleLike(this.member().id).subscribe({
+    next:_=>{
+      if(this.hasLiked()){
+          this._likesServices.likeIds.update(ids=>ids.filter(x=>x !==this.member().id));
+      }
+      else{
+            this._likesServices.likeIds.update(ids=>[...ids,this.member().id]);
+      }
+    }
+  })
+}
 
 }
